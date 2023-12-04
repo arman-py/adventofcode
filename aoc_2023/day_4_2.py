a53867fc26b3c0f174290e2d5a8dec146824f672
Arman <armanhhrayri@gmail.com>
2023-12-04 09:59:38 +0400

    Create day_4_2.py
    2023 Day 4 Part 2

---
+++ aoc_2023/day_4_2.py
@@ -0,0 +1,20 @@
+data = """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"""
+
+
+if __name__ == "__main__":
+    game_data = data.split("\n")
+    game_info = {x: 1 for x in range(1, len(game_data) + 1)}
+    for line in game_data:
+        game_id = int(line[5:].split(":")[0])
+        wining_part, my_part = line[5:].split(":")[1][1:].split("|")
+        wining_part = wining_part.split()
+        my_part = my_part.split()
+        wining_count = len(set(my_part) & set(wining_part))
+        for x in range(1, wining_count + 1):
+            game_info[game_id + x] += game_info[game_id]
+    print(sum(game_info.values()))
